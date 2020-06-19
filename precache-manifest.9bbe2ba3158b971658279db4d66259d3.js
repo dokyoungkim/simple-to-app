f377702ef294971b0d6b2eb5fc04bdbd024c09d3
dokyoungkim <thirdparty@daum.net>
2020-06-19 14:00:33 +0900

    Updates

---
+++ precache-manifest.9bbe2ba3158b971658279db4d66259d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac1dbe0582b14fefac6affb59fd16d8a",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "7024c79352456b6f93cd",
+    "url": "/simple-todo-app/static/css/main.0662ecdc.chunk.css"
+  },
+  {
+    "revision": "b4ec96c8af681f6dcf28",
+    "url": "/simple-todo-app/static/js/2.f892e1d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.f892e1d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7024c79352456b6f93cd",
+    "url": "/simple-todo-app/static/js/main.c5ba542a.chunk.js"
+  },
+  {
+    "revision": "60352d12923d0b224660",
+    "url": "/simple-todo-app/static/js/runtime-main.d4086948.js"
+  }
+]);
